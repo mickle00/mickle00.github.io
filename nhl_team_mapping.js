57fad77b405994c245fdbed8251b641d0c2f824a
Michael Stewart <mickle00@gmail.com>
2016-10-14 07:18:31 -0700

    changing for 2016-2017 season

--- nhl_team_mapping.js
+++ nhl_team_mapping.js
@@ -1,32 +1,32 @@
 var TEAM_OWNERS = {
-   'Montreal'       :   'Andy'
-  ,'Tampa Bay'      :   'Kevin'
-  ,'Florida'        :   'Alex'
-  ,'Ottawa'         :   'Mike'
-  ,'Detroit'        :   'Alex'
-  ,'Boston'         :   'Mike'
-  ,'Buffalo'        :   'Matt'
-  ,'Toronto'        :   'Alex'
-  ,'NY Islanders'   :   'Kevin'
-  ,'NY Rangers'     :   'Mike'
-  ,'Washington'     :   'Matt'
-  ,'Pittsburgh'     :   'Duncan'
-  ,'Philadelphia'   :   'Andy'
-  ,'New Jersey'     :   'Matt'
-  ,'Carolina'       :   'Duncan'
-  ,'Columbus'       :   'Kevin'
-  ,'Dallas'         :   'Duncan'
-  ,'Nashville'      :   'Kevin'
-  ,'St. Louis'      :   'Andy'
-  ,'Winnipeg'       :   'Alex'
-  ,'Minnesota'      :   'Mike'
-  ,'Chicago'        :   'Duncan'
-  ,'Colorado'       :   'Mike'
-  ,'San Jose'       :   'Andy'
-  ,'Vancouver'      :   'Matt'
-  ,'Arizona'        :   'Kevin'
-  ,'Los Angeles'    :   'Duncan'
-  ,'Edmonton'       :   'Andy'
-  ,'Anaheim'        :   'Matt'
-  ,'Calgary'        :   'Alex'
+   'Montreal'       :  [ 'Kevin', false,'$14']
+  ,'Tampa Bay'      :  [ 'Alex', false,'$18']
+  ,'Florida'        :  [ 'Kevin', false,'$8']
+  ,'Ottawa'         :  [ 'Duncan', false,'$2']
+  ,'Detroit'        :  [ 'Alex', false,'$6']
+  ,'Boston'         :  [ 'Mike', true,'Favorite']
+  ,'Buffalo'        :  [ 'Matt', true,'Favorite']
+  ,'Toronto'        :  [ 'Alex', true,'Favorite']
+  ,'NY Islanders'   :  [ 'Mike', false,'$9']
+  ,'NY Rangers'     :  [ 'Duncan', false,'$13']
+  ,'Washington'     :  [ 'Matt', false,'$20']
+  ,'Pittsburgh'     :  [ 'Mike', false,'$19']
+  ,'Philadelphia'   :  [ 'Andy', false,'$3']
+  ,'New Jersey'     :  [ 'Matt', false,'$1']
+  ,'Carolina'       :  [ 'Alex', false,'$0']
+  ,'Columbus'       :  [ 'Kevin', true,'Favorite']
+  ,'Dallas'         :  [ 'Matt', false,'$8']
+  ,'Nashville'      :  [ 'Alex', false,'$16']
+  ,'St. Louis'      :  [ 'Andy', false,'$18']
+  ,'Winnipeg'       :  [ 'Kevin', false,'$1']
+  ,'Minnesota'      :  [ 'Mike', false,'$7']
+  ,'Chicago'        :  [ 'Duncan', false,'$20']
+  ,'Colorado'       :  [ 'Mike', false,'$5']
+  ,'San Jose'       :  [ 'Andy', true,'Favorite']
+  ,'Vancouver'      :  [ 'Matt', false,'$1']
+  ,'Arizona'        :  [ 'Kevin', false,'$2']
+  ,'Los Angeles'    :  [ 'Duncan', true,'Favorite']
+  ,'Edmonton'       :  [ 'Duncan', false,'$4']
+  ,'Anaheim'        :  [ 'Andy', false,'$16']
+  ,'Calgary'        :  [ 'Andy', false,'$3']
 };
